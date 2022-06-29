065beec8ff46b58842a1c5c01a72563f15632035
Aysel <aysel2920@gmail.com>
2022-06-29 12:46:51 +0200

    some updates to ContentView

--- TaskManager/View/ContentView.swift
+++ TaskManager/View/ContentView.swift
@@ -15,20 +15,20 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) var viewContext
     @FetchRequest(entity: Task.entity(), sortDescriptors: []) var tasks: FetchedResults<Task>
     
-    init() {
-        UINavigationBar.appearance().backgroundColor = .black
-    }
-    
     var body: some View {
         
         NavigationView {
+            
+            //List of all tasks
             List {
                 ForEach(tasks, id: \.self) { task in
                     NavigationLink(destination: TaskDetails(task: task)){
+                        
                         VStack(alignment: .leading){
                             Text(task.wrappedTitle)
                                 .font(.title2.bold())
                                 .lineLimit(1)
+                            
                             HStack{
                                 Text(task.wrappedDate,style: .date)
                                 Text(task.wrappedDescription)
@@ -39,11 +39,11 @@ struct ContentView: View {
                         }
                         .padding(5)
                     }
+                //Swap to delete task
                 }.onDelete(perform: deleteTask)
             }
             .onAppear(perform: loadDefaultTasks)
             .navigationBarTitle("Tasks",displayMode: .large)
-            .navi
             .listStyle(PlainListStyle())
             .toolbar{
                     ToolbarItemGroup(placement: .bottomBar) {
@@ -62,6 +62,33 @@ struct ContentView: View {
         }
           
     }
+    
+    //Add new task
+    private func addTask(){
+        let newTask = Task(context: viewContext)
+        newTask.title = title
+        newTask.date = Date.now
+        newTask.desciption = description
+        newTask.comments = []
+
+        try? self.viewContext.save()
+    }
+    
+    //Delete task
+    private func deleteTask(offsets: IndexSet) {
+        withAnimation{
+            offsets.map {tasks[$0]}.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch{
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    //Function to have one default task when we start app
     private func loadDefaultTasks(){
         
         if tasks.count == 0 {
@@ -78,30 +105,6 @@ struct ContentView: View {
         task1.comments = [cmnt1,cmnt2]
         
         try? self.viewContext.save()
-        }
-    }
-    
-    private func addTask(){
-        
-        let task1 = Task(context: viewContext)
-        task1.title = title
-        task1.date = Date.now
-        task1.desciption = description
-        
-        task1.comments = []
-        
-        try? self.viewContext.save()
-    }
-    private func deleteTask(offsets: IndexSet) {
-        withAnimation{
-            offsets.map {tasks[$0]}.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch{
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
         }
     }
 }
